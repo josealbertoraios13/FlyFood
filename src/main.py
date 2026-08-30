58c58d71015986d6f7c1597127640709e1234e8d
DgDavi <davi46796@gmail.com>
2026-08-30 14:59:07 -0300

    refactor: reorganizei as pastas e fiz a api retornar os pontos no formato correto.

--- src/main.py
+++ src/main.py
@@ -1,28 +1,8 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
 
-app = FastAPI()
+from routes import router
 
-class MatrizInput(BaseModel):
-    matriz: List[List[str]]
+app = FastAPI(title="FLyFood API")
+app.include_router(router)
 
-
-@app.post("/receber-matriz")
-async def receber_matriz(dados: MatrizInput):
-    """
-    Recebe a matriz no seguinte formato:
-
-    {
-    "matrix": [
-        ["R", ".", ".", "P"],
-        [".", ".", ".", "."],
-        ["P", ".", ".", "P"]
-    ]
-    }
-
-    """
-    linhas = len(dados.matriz)
-    colunas = len(dados.matriz[0]) if linhas > 0 else 0
-    return {"linhas": linhas, "colunas": colunas}
-
+# uvicorn main:app --reload
---
+++ src/routes.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, HTTPException
+from model.matrix_model import MatrixInput, MatrixResponse
+
+from services import matriz_para_pontos
+
+router = APIRouter()
+
+@router.get("/")
+async def root():
+    return {"message": "API está funcionando!"}
+
+
+@router.post("/flyfood", response_model=MatrixResponse)
+async def obter_pontos(dados: MatrixInput):
+    """
+    Endpoint para processar a matriz de entrada e retornar os pontos correspondentes.
+
+    Exemplo de entrada:
+    {
+  "matrix": [
+    [".", "I", ".", "P"],
+    [".", ".", "P", "."],
+    ["P", ".", ".", "."],
+    [".", ".", "P", "."]
+  ]
+}
+    """
+    try:
+        return matriz_para_pontos(dados.matrix)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
