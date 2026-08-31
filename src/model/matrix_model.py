efeef9b4aedf07d36c2a7db1f4fee6bb9f2fe319
DgDavi <davi46796@gmail.com>
2026-08-31 12:35:29 -0300

    Reapply "Update matrix_model.py"
    This reverts commit 36b5b882991eafd2d485ed07967152f3ef01e018.

--- src/model/matrix_model.py
+++ src/model/matrix_model.py
@@ -21,3 +21,24 @@ class Ponto(BaseModel):
 class MatrixResponse(BaseModel):
     matrix: List[List[Optional[Ponto]]]
     start: Ponto
+
+
+class Rota(BaseModel):
+    caminho: List[Ponto]
+    distancia: float
+
+
+class RotasInput(BaseModel):
+    rotas: List[Rota]
+
+    @field_validator("rotas")
+    @classmethod
+    def validate_rotas(cls, value):
+        if not value:
+            raise ValueError("A lista de rotas não pode estar vazia.")
+        return value
+
+
+class MenorRotaResponse(BaseModel):
+    caminho: List[Ponto]
+    distancia: float
