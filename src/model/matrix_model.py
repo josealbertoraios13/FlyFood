383cb2ead50fe3338408ff0ee31c1bee22833b90
rivanbarroso0 <rivanbarroso0@gmail.com>
2026-08-31 18:19:00 -0300

    Update matrix_model.py

--- src/model/matrix_model.py
+++ src/model/matrix_model.py
@@ -1,9 +1,9 @@
 from typing import List, Optional
 from pydantic import BaseModel, field_validator
-
+ 
 class MatrixInput(BaseModel):
     matrix: List[List[str]]
-
+ 
     @field_validator("matrix")
     @classmethod
     def validate_matrix(cls, value):
@@ -13,32 +13,11 @@ class MatrixInput(BaseModel):
         if any(len(linha) != largura for linha in value):
             raise ValueError("Todas as linhas da matriz devem ter o mesmo tamanho.")
         return value
-
+ 
 class Ponto(BaseModel):
     x: int
     y: int 
-
+ 
 class MatrixResponse(BaseModel):
     matrix: List[List[Optional[Ponto]]]
     start: Ponto
-
-
-class Rota(BaseModel):
-    caminho: List[Ponto]
-    distancia: float
-
-
-class RotasInput(BaseModel):
-    rotas: List[Rota]
-
-    @field_validator("rotas")
-    @classmethod
-    def validate_rotas(cls, value):
-        if not value:
-            raise ValueError("A lista de rotas não pode estar vazia.")
-        return value
-
-
-class MenorRotaResponse(BaseModel):
-    caminho: List[Ponto]
-    distancia: float
