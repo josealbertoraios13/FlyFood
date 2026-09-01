a5408635ee1677019e649f5f6257486d5c9e1f7b
Rivan Barroso <rivanbarroso0@gmail.com>
2026-09-01 14:44:41 -0300

    Delete src directory

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-from routes import router
-
-app = FastAPI(title="FLyFood API")
-app.include_router(router)
-
-# uvicorn main:app --reload
--- src/model/rotas model.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from pydantic import BaseModel
- 
-from model.matrix_model import Ponto
- 
- 
-class Rota(BaseModel):
-    caminho: List[Ponto]
-    distancia: float
