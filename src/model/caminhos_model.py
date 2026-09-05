bd7e3497d8a547a9b9490497071c655b2dfcbee3
jose-alberto <albertofreconvick@gmail.com>
2026-09-05 09:05:39 -0300

    Troquei a implementação de List e Optional (que era ultrapassada) para list -> caminhos_model.py

--- src/model/caminhos_model.py
+++ src/model/caminhos_model.py
@@ -1,11 +1,10 @@
-from typing import List
 from pydantic import BaseModel
 from model.matrix_model import Ponto
 
 class CaminhoCalculado(BaseModel):
-    caminho: List[Ponto]
+    caminho: list[Ponto]
     distancia: float
 
 class MenorCaminhoResponse(BaseModel):
-    caminho: List[Ponto]
+    caminho: list[Ponto]
     distancia: float
