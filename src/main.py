19556e4cde07612852794c7bc65390522ce355cb
DgDavi <davi46796@gmail.com>
2026-09-15 18:27:50 -0300

    feat: criei um função que pergunta se o usuário quer ver as informações do pc.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 
 from routes import carregar_matrix
-from utils import UtilsCPU
+from utils import InfoPC
 
 CAMINHO_JSON = Path(__file__).parent.parent / "matrix.json"
 
 if __name__ == "__main__":
     resultado = carregar_matrix(CAMINHO_JSON)
-    UtilsCPU().info_pc()
-        
 
     print(f"Rota: {resultado.rota}")
     print(f"Distância: {resultado.distancia} dronômetros")
     print(f"Tempo de resolução: {resultado.tempo_resolucao}s")
+
+    InfoPC().perguntar_exibir_info()
--- src/utils/info_utils.py
+++ src/utils/info_utils.py
@@ -1,19 +1,26 @@
 import platform
 import psutil
 
-class UtilsCPU:
+class InfoPC:
+    @staticmethod
+    def perguntar_exibir_info():
+        resposta = input("\nDeseja exibir as informações do PC? (s/n): ").strip().lower()
+        if resposta in ("s", "sim"):
+            print()
+            InfoPC.info_pc()
+
     @staticmethod
     def info_pc():
         print(f"Sistema Operacional: {platform.system()}")
-        print(f"Versão do SO: {platform.version()}")
+        print(f"Versão do SO: {platform.version()}\n")
 
         print(f"CPU: {platform.processor()}")
-        print(f"Uso da CPU: {psutil.cpu_percent()}")
+        print(f"Uso da CPU: {psutil.cpu_percent()}\n")
 
         memoria = psutil.virtual_memory()
         print(f"RAM Total: {memoria.total / (1024**3):.2f} GB")
         print(f"Usada: {memoria.used / (1024**3):.2f} GB")
-        print(f"Uso: {memoria.percent}%")
+        print(f"Uso: {memoria.percent}%\n")
 
         for processo in psutil.process_iter(['pid', 'name', 'memory_percent']):
             try:
