16c4b34f12f3ad024c96078d37337481559728e3
Kauan Carreiro <kauan.h.carreiro@gmail.com>
2026-09-15 23:16:04 -0300

    refactor: chama calcular_caminhos dentro de gerar_possibilidades

--- src/utils/matrix_utils.py
+++ src/utils/matrix_utils.py
@@ -1,6 +1,7 @@
 from itertools import permutations
 
 from model import MatrixResponse, Ponto
+from utils.caminho_utils import CaminhoUtils
 
 class MatrixUtils:
     @staticmethod
@@ -58,4 +59,6 @@ class MatrixUtils:
             caminho = [matrix_response.start, *permutacao]
             possibilidades.append(caminho)
 
+        caminhos_calculados = CaminhoUtils.calcular_caminhos(caminhos=possibilidades)
+        
         return possibilidades
